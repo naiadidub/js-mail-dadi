fa86b2fba62e6eb5860943c28e7016d2596765f3
naiadidub <antonio.zaghi1405@gmail.com>
2022-03-31 16:09:17 +0200

    x simone

--- mail/script.js
+++ mail/script.js
@@ -16,28 +16,28 @@ for (let i = 0; i < listchar.length; i++){
         }
     }
 }  */
-let flag = false
 let email = prompt('inserisci email')
-if (email == ''){
-    alert('nessuna email inserita')
-    flag = true
-}
 
-if(flag == false){
     let elencoemail = ['pippo@gmail.com', 'pluto@gmail.com', 'baudo@gmail.com','checco@gmail.com', 'paolo@gmail.com',]
     for (let i = 0; i < elencoemail.length; i++){
         if (email !== elencoemail[i]){
-            console.log('email non trovata')
-            let c = document.createElement("p")
-            c.innerText = "email non trovata"
-            document.getElementById('miodiv').innerHTML = c.innerText = "email non trovata"
+            let flag 
+            flag = false
+            
         } else {
-            console.log('email trovata')
-            let b = document.createElement("p")
-            b.innerText = "email trovata"
-            document.getElementById('miodiv').innerHTML = b.innerText = "email trovata"
+            flag = true
         }
     }
-}
 
+if (flag = false){
+    console.log('email non trovata')
+    let c = document.createElement("p")
+    c.innerText = "email non trovata"
+    document.getElementById('miodiv').innerHTML = c.innerText = "email non trovata"
+} else {
+    console.log('email trovata')
+    let b = document.createElement("p")
+    b.innerText = "email trovata"
+    document.getElementById('miodiv').innerHTML = b.innerText = "email trovata"
+}
 
